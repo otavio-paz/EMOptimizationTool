229955be006a394d68e67adc14d8bd92f671e4a2
otavio-paz <pazotavio.n@gmail.com>
2023-11-20 02:00:26 -0500

    Added mode 2 implementation

--- src/EnergyManagerFormatter.py
+++ src/EnergyManagerFormatter.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import sys
 
 # Get the current directory
 current_directory = os.getcwd()
@@ -16,7 +17,8 @@ else:
         print("Select a mode:")
         print("1 - Buildings in separate .csv files.")
         print("2 - All buildings in one file")
-        mode_choice = input("Choice (1 or 2): ")
+        print("3 - Quit")
+        mode_choice = input("Choice (1, 2, or 3): ")
 
         # Columns to remove
         columns_to_remove = [5, 6, 9, 10, 11, 12, 13, 14, 15, 16]
@@ -48,7 +50,43 @@ else:
         elif mode_choice == "2":
 
             # Mode 2: Buildings are in the same .csv file (vertically positioned)
-            print("To do!")
+            all_buildings_file = "all_buildings.csv"
+
+            with open(all_buildings_file, 'w', newline='') as output_file:
+                csv_writer = csv.writer(output_file)
+                previous_building = None
+                empty_lines_added = 0
+
+                for csv_file in csv_files:
+                    input_file_path = os.path.join(current_directory, csv_file)
+
+                    with open(input_file_path, 'r') as input_file:
+                        csv_reader = csv.reader(input_file)
+                        lines = list(csv_reader)
+
+                        for line in lines:
+                            # Remove specified columns
+                            modified_line = [line[i] for i in range(len(line)) if i not in columns_to_remove]
+
+                            # Check condition for Natural Gas and skip the line
+                            if modified_line[2].lower() == "natural gas":
+                                continue
+
+                            # Check if the building changes
+                            if line[0] != previous_building:
+                                if previous_building is not None:
+                                    # Add 5 empty lines if the building changes
+                                    for _ in range(5):
+                                        csv_writer.writerow([])
+                                empty_lines_added = 0
+                                previous_building = line[0]
+
+                            csv_writer.writerow(modified_line)
+
+                print(f"Columns removed, Natural Gas lines skipped, and output saved to {all_buildings_file}")
+
+        elif mode_choice == "3":
+            sys.exit()
 
         else:
-            print("Invalid mode choice. Please enter 1 or 2.")
+            print("Invalid mode choice. Please enter 1, 2, or 3.")
